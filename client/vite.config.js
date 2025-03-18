b1a6614eaa685eb5eba4f951446ee3f29edb14f9
Abhishek Singh <abhishekps5171@gmail.com>
2025-03-18 17:06:03 +0530

    complete sign up page functionality

--- client/vite.config.js
+++ client/vite.config.js
@@ -9,4 +9,13 @@ export default defineConfig({
     tailwindcss(),
     react()
   ],
+  server:{
+    proxy:{
+      '/api':
+      {
+        target:'http://localhost:3000',
+        secure: false,
+      },
+    },
+  },
 })
