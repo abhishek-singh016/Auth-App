ac16288a8fbc66afdcd42e033535cd2d3a704f2c
Abhishek Singh <abhishekps5171@gmail.com>
2025-03-21 01:41:23 +0530

    update header component and make profile private

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,57 +1,88 @@
-// import {Link} from 'react-router-dom';
-
-// export default function Header() {
-//   return (
-//     <div className='bg-slate-200'>
-//         <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
-//             <Link to='/'>
-//             <h1 className='font-bold'>Auth App</h1>
-//             </Link>
-//             <ul className='flex gap-4'>
-//                 <Link to='/'>
-//                 <li>Home</li>
-//                 </Link>
-                
-//                 <Link to='/about'>
-//                 <li>About</li>
-//                 </Link>
-                
-//                 <Link to='/sign-in'>
-//                 <li>Sign In</li>
-//                 </Link>
-            
-//             </ul>
-//         </div>
-//     </div>
-//   )
-// }
-
-
-import { Link } from 'react-router-dom';
+import {Link} from 'react-router-dom';
+import {useSelector} from 'react-redux';
 
 export default function Header() {
+const{currentUser} = useSelector(state => state.user);
   return (
-    <div className="bg-slate-200">
-      <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
-        {/* Logo */}
-        <Link to="/">
-          <h1 className="font-bold text-xl">Auth App</h1>
-        </Link>
+    <div className='bg-slate-200'>
+        <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
+            <Link to='/'>
+            <h1 className='font-bold'>Auth App</h1>
+            </Link>
+            <ul className='flex gap-4'>
+                <Link to='/'>
+                <li>Home</li>
+                </Link>
 
-        {/* Navigation */}
-        <ul className="flex gap-6 text-lg">
-          <li>
-            <Link to="/" className="hover:text-blue-600 transition duration-200">Home</Link>
-          </li>
-          <li>
-            <Link to="/about" className="hover:text-blue-600 transition duration-200">About</Link>
-          </li>
-          <li>
-            <Link to="/sign-in" className="hover:text-blue-600 transition duration-200">Sign In</Link>
-          </li>
-        </ul>
-      </div>
+                <Link to='/about'>
+                <li>About</li>
+                </Link>
+
+                <Link to='/profile'>
+                {currentUser ? (
+                  <img src={currentUser.profilePicture} alt="profile"  className= 'h-7 w-7 rounded-full object-cover'/>
+                ) : (
+                    <li>Sign In</li>
+                  )
+                }
+
+                </Link>
+
+            </ul>
+        </div>
     </div>
-  );
+  )
 }
 
+// import { Link } from "react-router-dom";
+// import { useSelector } from "react-redux";
+
+// export default function Header() {
+//   const { currentUser } = useSelector((state) => state.user);
+//   return (
+//     <div className="bg-slate-200">
+//       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+//         {/* Logo */}
+//         <Link to="/">
+//           <h1 className="font-bold text-xl">Auth App</h1>
+//         </Link>
+
+//         {/* Navigation */}
+//         <ul className="flex gap-6 text-lg">
+//           <li>
+//             <Link
+//               to="/"
+//               className="hover:text-blue-600 transition duration-200"
+//             >
+//               Home
+//             </Link>
+//           </li>
+//           <li>
+//             <Link
+//               to="/about"
+//               className="hover:text-blue-600 transition duration-200"
+//             >
+//               About
+//             </Link>
+//           </li>
+//           <li>
+//             <Link
+//               to="/sign-in"
+//               className="hover:text-blue-600 transition duration-200"
+//             >
+//               {currentUser ? (
+//                 <img
+//                   src={currentUser.profilePicture}
+//                   alt="Profile"
+//                   className="w-8 h-8 rounded-full border-2 border-gray-300"
+//                 />
+//               ) : (
+//                 "Sign In"
+//               )}
+//             </Link>
+//           </li>
+//         </ul>
+//       </div>
+//     </div>
+//   );
+// }
